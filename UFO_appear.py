0354b9f88c39528e2270f1bbf5512b36e9ee74c3
Kenan Begic <kenan.begic.2002@gmail.com>
2026-03-12 20:37:13 +0100

    lab 3 Vjezba analize podataka o nlo

---
+++ UFO_appear.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import plotly.express as px
+from matplotlib import pyplot as plt
+
+df = pd.read_csv('ufo_sightings.csv', low_memory=False)
+
+shape_count = df["shape"].value_counts().head(10)
+
+shape_count.plot(kind="bar")
+
+plt.title("Most Common UFO Shapes")
+plt.xlabel("Shapes")
+plt.ylabel("Number of Sinhtings")
+
+plt.show()
